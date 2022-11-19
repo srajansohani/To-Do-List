83708332e6207c4668630d3e2b6c237ba8a9a70a
srajansohani <srajansohani999@gmail.com>
2022-11-19 22:47:04 +0530

    hello

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+const port = process.env.PORT;
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 var Items = [];
@@ -40,6 +40,6 @@ app.post('/', (req, res) => {
 
 })
 
-app.listen(3009, () => {
+app.listen(port, () => {
     console.log('listening on port 3009');
 })
