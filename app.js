3148cc6bd297fe8c018e5f2c44a387764528a8bc
srajansohani <srajansohani999@gmail.com>
2022-12-14 14:46:28 +0530

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static("public"));
-const port = process.env.PORT || 3009; //port is enviorment varible as it would be updated by host - heroku 
+const port = process.env.PORT; //port is enviorment varible as it would be updated by host - heroku 
 
 
 //setupping bodyParser
